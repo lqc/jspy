d56334b8b07c2d541e702f3e23d38009c8602f69
Łukasz Rekucki <lrekucki@gmail.com>
2011-07-03 00:03:03 +0200

    Make PLY an external package instead of bundling it.

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import sys
 from setuptools import setup
 
 setup_requires = []
-install_requires = []
+install_requires = ["PLY>=3.4"]
 
 if sys.version_info < (2,7):
     setup_requires.append("unittest2")
@@ -19,7 +19,7 @@ setup(
     setup_requires=setup_requires,
     install_requires=install_requires,
     platforms='Cross Platform',
-    packages=['jspy', 'ply'],
+    packages=['jspy'],
     scripts=['scripts/jspy'],
     test_suite='jspy.compat.test_collector',
 )
